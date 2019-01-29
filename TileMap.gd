8ba2ae50322200f7916892a3368e7060c7dd692f
Zireael07 <zi@mail.com>
2019-01-29 19:43:27 +0100

    new: build astar map from the map made in Godot editor

--- TileMap.gd
+++ TileMap.gd
@@ -6,11 +6,27 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Astar representation
+	Astar_map.build_map(Vector2(get_used_rect().end.x, get_used_rect().end.y), get_floor_cells())
+	
+	
+	#pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func get_floor_cells():
+	var rect : Rect2 = get_used_rect()
+	var list = []
+	var vec : Vector2
+	for x in range( rect.end.x ):
+		for y in range( rect.end.y ):
+			vec = Vector2(x,y)
+			if is_walkable(vec):
+				list.append(vec)
+	
+	return list
 
 func get_entities_in_cell(cell):
 	var list = []
@@ -20,9 +36,9 @@ func get_entities_in_cell(cell):
 	return list
 
 # Return TRUE if cell is a floor on the map
-func is_walkable( cell ):
+func is_walkable(cell: Vector2):
 	var walk = [0,1,3]
-	return walk.has(get_cellv( cell ))
+	return walk.has(get_cellv(cell))
 	
 # Return False if cell is an unblocked floor
 # Return Object if cell has a blocking Object
@@ -34,3 +50,14 @@ func is_cell_blocked(cell):
 			blocks = e
 
 	return blocks
+	
+# MAIN LOOP!!!
+# Turn-based
+func _on_player_acted():
+	for node in get_tree().get_nodes_in_group('entity'):
+		if node != RPG.player and node.ai:
+			#print(node.get_name() + " gives you a dirty look!")
+			node.ai.take_turn()
+			
+func _on_player_pos_changed(player):
+	pass
