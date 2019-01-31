4586bb0c2cc0421fa4586cb234ec297468b8c951
Zireael07 <zi@mail.com>
2019-01-31 19:41:34 +0100

    new: random rolls

--- RPG.gd
+++ RPG.gd
@@ -7,6 +7,16 @@ var player
 func _ready():
 	pass # Replace with function body.
 
+func roll(l,h):
+	return int(round(rand_range(l,h)))
+
+func roll_dice(n, d):
+	var res = 0
+	for i in range(n):
+		res += int(round(rand_range(1,d)))
+		
+	return res
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
