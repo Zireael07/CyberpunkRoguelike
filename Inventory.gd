b06e353ce7460bcbf1908e8892f213339e8546de
Zireael07 <zi@mail.com>
2019-02-18 19:27:11 +0100

    new: copy over the inventory GUI

---
+++ Inventory.gd
@@ -0,0 +1,109 @@
+extends HBoxContainer
+
+# class member variables go here, for example:
+onready var objects = get_node('../InventoryObjects')
+onready var eq = get_node("../VBoxContainer/EquipmentContainer")
+onready var name_label = get_node('../VBoxContainer/ItemName')
+
+
+# Get an array of all inventory Objects
+func get_objects():
+	return self.objects.get_children()
+
+func get_equipped_objects():
+	var eq = []
+	var ob = get_objects()
+	for o in ob:
+		if o.item.equipped:
+			eq.append(o)
+	
+	for o in eq:
+		print(o.get_name())
+	return eq
+
+
+# Get the first free inventoryslot
+func get_free_slot():
+	for node in get_children():
+		if node.contents.empty():
+			return node
+
+# find an inventoryslot which contains an item
+func get_matching_slot(item):
+	for node in get_children():
+		if not node.contents.empty():
+			if node.contents[0].name == item.name:
+				return node
+
+# add an item to an inventoryslot
+func add_to_inventory(item):
+	# find a matching slot
+	var slot = get_matching_slot(item)
+	# find free slot if no matches found
+	if !slot: slot = get_free_slot()
+	# break if no slots free
+	if !slot: return
+	
+	# handled by container script
+	# remove from world objects group
+#	if item.is_in_group('entity'):
+#		item.remove_from_group('entity')
+#	# add to inventory group
+#	if not item.is_in_group('inventory'):
+#		item.add_to_group('inventory')
+
+	# shift item parent from Map to InventoryObjects
+	#item.get_parent().remove_child(item)
+	#objects.add_child(item)
+	
+	# assign the item to the slot
+	slot.add_contents(item)
+
+func remove_from_inventory(slot, item):
+	slot.remove_contents(item)
+
+	# handled by the container script	
+#	item.remove_from_group('inventory')
+#	item.add_to_group('entity')
+#
+#	item.get_parent().remove_child(item)
+#	RPG.map.add_child(item)
+	item.set_map_position(RPG.player.get_map_position())
+	
+func move_to_equipped(slot, equip_slot, item):
+#	print("Slot: " + str(slot) + " " + str(equip_slot))
+	if equip_slot == "BODY":
+	#	if eq.get_child(0).contents.empty():
+		print("Equipping")
+		eq.get_child(0).add_contents(item)
+		slot.remove_contents(item)
+	#	else:
+	#		print("Slot taken")
+	elif equip_slot == "MAIN_HAND":
+	#	if eq.get_child(1).contents.empty():
+		print("Equipping")
+		eq.get_child(1).add_contents(item)
+		slot.remove_contents(item)
+	#	else:
+	#		print("Slot taken")
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	
+	RPG.inventory = self
+
+# moue support
+func _on_slot_mouse_enter(slot):
+	var name = '' if slot.contents.empty() else slot.contents[0].name
+	var count = slot.contents.size()
+	var nt = '' if count < 2 else str(count)+'x '
+	name_label.set_text(nt + name)
+
+func _on_slot_mouse_exit():
+	name_label.set_text('')
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
---
+++ InventorySlot.gd
@@ -0,0 +1,86 @@
+extends Button
+
+# class member variables go here, for example:
+var inven_root = null	
+	
+var contents = []
+
+func add_contents(what):
+	contents.append(what)
+	what.item.inventory_slot = self
+	
+	update_slot()
+
+func remove_contents(what):
+	contents.remove(contents.find(what))
+	what.item.inventory_slot = null
+	update_slot()
+
+
+func update_slot():
+	if not contents.empty():
+		get_node('Sprite').set_texture(contents[0].get_icon())
+		set_disabled(false)
+	else:
+		get_node('Sprite').set_texture(null)
+		set_disabled(true)
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	
+	inven_root = get_tree().get_nodes_in_group("inventory_gui")[0]
+	
+	connect("mouse_entered", get_parent(), "_on_slot_mouse_enter", [self])
+	connect("mouse_exited", get_parent(), "_on_slot_mouse_exit")
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_InventorySlot_pressed():
+	# don't show if nothing in slot
+	if contents.size() > 0:
+		if get_node("MenuContainer").is_visible():
+			get_node("MenuContainer").hide()
+		else:
+			# hide use button if wielded
+			if contents.size() > 0 and contents[0].item.equipped:
+				print("Hiding use button because equipped: " + str(contents[0].item.get_parent().get_name()))
+				get_node("MenuContainer/UseButton").hide()
+			# show the menu
+			get_node("MenuContainer").show()
+			
+
+
+func _on_DropButton_pressed():
+	var obj = contents[0]
+	obj.item.drop(RPG.player)
+	update_slot()
+	
+	get_node("MenuContainer").hide()
+	
+	# close the whole inventory
+	inven_root.hide()
+	# unpause
+	get_tree().set_pause(false)
+
+
+func _on_UseButton_pressed():
+	var obj = contents[0]
+	obj.item.use(RPG.player)
+	update_slot()
+	
+	get_node("MenuContainer").hide()
+	
+	# close the whole inventory
+	inven_root.hide()
+	# unpause
+	get_tree().set_pause(false)
+	
+	
+	#pass # replace with function body
